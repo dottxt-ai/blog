5503ba4b6513fffbc676e6fae18d9ab6527a19c9
Rémi Louf <remilouf@gmail.com>
2024-02-01 16:31:35 +0100

    Add mailing list

--- publish.el
+++ publish.el
@@ -54,7 +54,20 @@
          :section-numbers nil
          :htmlized-source t
          :with-toc nil
-         :html-postamble nil
+         :html-postamble "<div id='mc_embed_shell'>
+                <div id='mc_embed_signup'>
+                <form action='https://dottxt.us12.list-manage.com/subscribe/post?u=aec344375257ff07dcf9a6ef6&amp;id=922982b042&amp;f_id=00db4ee0f0' method='post' id='mc-embedded-subscribe-form' name='mc-embedded-subscribe-form' class='validate' target='_self' novalidate=''>
+                        <div id='mc_embed_signup_scroll'><h2>Keep up with structured generation</h2>
+                        <div class='indicates-required'><span class='asterisk'>*</span> indicates required</div>
+                        <div class='mc-field-group'><label for='mce-EMAIL'>Email Address <span class='asterisk'>*</span></label><input type='email' name='EMAIL' class='required email' id='mce-EMAIL' required='' value=''><span id='mce-EMAIL-HELPERTEXT' class='helper_text'></span></div>
+                        <div id='mce-responses' class='clear'>
+                        <div class='response' id='mce-error-response' style='display: none;'></div>
+                        <div class='response' id='mce-success-response' style='display: none;'></div>
+                        </div><div aria-hidden='true' style='position: absolute; left: -5000px;'><input type='text' name='b_aec344375257ff07dcf9a6ef6_922982b042' tabindex='-1' value=''></div><div class='clear'><input type='submit' name='subscribe' id='mc-embedded-subscribe' class='button' value='Subscribe'></div>
+                </div>
+                </form>
+                </div>
+                </div>"
          :auto-sitemap t
          :sitemap-title ".txt engineering"
          :sitemap-filename "index.org"
